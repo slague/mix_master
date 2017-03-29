9b690f37e24326bbb49019d9786b8c7bb81b6b56
slague <stephanielague@gmail.com>
2017-03-29 16:44:38 -0600

    completes feature test for user_creates_playlist

---
+++ app/models/playlist.rb
@@ -0,0 +1,5 @@
+class Playlist < ActiveRecord::Base
+
+  has_many :playlist_songs
+  has_many :songs, through: :playlist_songs
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
   resources :artists do
     resources :songs, only: [:new, :create]
   end
+
   resources :songs, only: [:show]
+  resources :playlists, only: [:index, :new, :create, :show]
+
 end
